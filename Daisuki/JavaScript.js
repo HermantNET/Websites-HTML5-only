c1c9099a11f95e202b210a67e81d5dedc94222f6
HermantNET <hermant.edu@gmail.com>
2016-07-23 22:00:42 -0400

    Daisuki: added API calls to replace all p tags and the clicked notice message.

---
+++ Daisuki/JavaScript.js
@@ -0,0 +1,31 @@
+﻿document.getElementById("clickMe").onclick = doFunction;
+document.getElementById("reveal").onclick = revealContent;
+
+var posts = "";
+
+function doFunction() {
+    var request = new XMLHttpRequest();
+    request.open('GET', 'http://jsonplaceholder.typicode.com/posts/1', false);
+    request.send();
+    var json = JSON.parse(request.responseText);
+    document.querySelector(".notice").textContent = json.title;
+};
+
+function itFunction() {
+    var request = new XMLHttpRequest();
+    request.open('GET', 'http://jsonplaceholder.typicode.com/posts', false);
+    request.send();
+    posts = JSON.parse(request.responseText);
+};
+
+itFunction();
+
+function revealContent() {
+    document.querySelector(".memes").classList.toggle("memes-open");
+};
+
+var para = document.querySelectorAll("p");
+
+for (var i = 0; 1 < para.length; i++) {
+    para[i].textContent = posts[i].title;
+};
